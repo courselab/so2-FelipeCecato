60cd8b2d110ec75a327be30b5ec53dc1f7932d10
Felipe Cecato <108101515+FelipeCecato@users.noreply.github.com>
2024-07-10 22:36:03 -0300

    mydos

--- mydos/kernel.h
+++ mydos/kernel.h
@@ -1,11 +1,13 @@
 /*
- *    SPDX-FileCopyrightText: 2024 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2024 FelipeCecato <fececato31@gmail.com>
  *   
  *    SPDX-License-Identifier: GPL-3.0-or-later
  *
- *    This file is part of SYSeg, available at https://gitlab.com/monaco/syseg.
+ *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
+ *  and contains modifications carried out by the following author(s):
+ *  FelipeCecato <fececato31@gmail.com>
  */
-
 #ifndef KERNEL_H
 #define KERNEL_H
 
